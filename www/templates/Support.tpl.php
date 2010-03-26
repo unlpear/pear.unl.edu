3caf5f67017ac44ae4d83cb714755d3c28bd1570
Brett Bieber <brett.bieber@gmail.com>
2010-03-26 21:06:53 +0000

    Add a custom support template with some more information.

---
+++ www/templates/Support.tpl.php
@@ -0,0 +1,17 @@
+<?php
+// Set the title for the main template
+$parent->context->page_title = 'Support | '.pear2\SimpleChannelFrontend\Main::$channel->name;
+?>
+<div class="support grid_8">
+    <h1>Support</h1>
+    <p>Most of the UNL PEAR packages were built  by members of the UNL <a href="http://wdn.unl.edu/">Web Developer Network</a>. First try to contact the maintainer of the individual package you're trying to use.
+   If there is no response or you're having trouble with the channel server itself, contact Brett Bieber.</p>
+    <p>For help installing PEAR packages:</p>
+    <ul>
+        <li>There's a Pyrus section in <a href="http://pear.php.net/manual/en/pyrus.php">PEAR manual</a>.</li>
+        <li>You can also ask for help on the <i><a href="irc://irc.freenode.net/#unl_wdn">#unl_wdn</a></i> 
+            <acronym title="Internet Relay Chat">IRC</acronym> channel on
+            <a href="http://freenode.net/">Freenode</a>.
+        </li>
+    </ul>
+</div>
